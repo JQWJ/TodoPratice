bfb40b37e9453309745a884b955f2409785cbc9c
DELBE <jangjeong214@naver.com>
2026-01-13 19:58:37 +0900

    Initial Commit

---
+++ TodoPratice/TodoPraticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoPraticeApp.swift
+//  TodoPratice
+//
+//  Created by 장정재 on 1/13/26.
+//
+
+import SwiftUI
+
+@main
+struct TodoPraticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
