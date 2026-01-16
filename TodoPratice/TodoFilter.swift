2c42350a4d7a51ed6d5e02ef2ea7e62696dc33a2
DELBE <jangjeong214@naver.com>
2026-01-16 21:22:24 +0900

    filter의 상태값 추가
    필터의 각 케이스 별로 전체, 완료, 미완료에 대한 케이스 생성

---
+++ TodoPratice/TodoFilter.swift
@@ -0,0 +1,16 @@
+//
+//  TodoFilter.swift
+//  TodoPratice
+//
+//  Created by 장정재 on 1/16/26.
+//
+
+import SwiftUI
+
+enum TodoFilter: String, CaseIterable, Identifiable {
+    case all = "All"
+    case done = "Done"
+    case todo = "Todo"
+    
+    var id: Self { self }
+}
