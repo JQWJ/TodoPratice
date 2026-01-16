e54291f0a95e44ad6749fd5ba0389c10999f0e58
DELBE <jangjeong214@naver.com>
2026-01-16 21:31:00 +0900

    save, load를 위한 상태값 추가 및 함수 생성
    생성한 todo를 저장하기 위한 코드

--- TodoPratice/TodoListViewModel.swift
+++ TodoPratice/TodoListViewModel.swift
@@ -10,21 +10,58 @@ import Combine
 
 final class TodoListViewModel: ObservableObject {
     
-    @Published var todos: [TodoItem] = []
+    @Published var todos: [TodoItem] = [] {
+        didSet {
+            saveTodos()
+        }
+    }
+    
+    @Published var filter: TodoFilter = .all
+    
+    var filteredTodos: [TodoItem] {
+        switch filter {
+        case .all:
+            return todos
+        case .done:
+            return todos.filter {$0.isDone}
+        case .todo:
+            return todos.filter {!$0.isDone}
+        }
+    }
+    
+    private let saveKey = "todos_key"
+    
+    init() {
+        loadTodos()
+    }
     
     func add(title: String) {
-        let newTodo = TodoItem(title: title, isDone: false)
-        todos.append(newTodo)
+        todos.append(TodoItem(id: UUID(), title: title, isDone: false))
     }
     
     func delete(at offsets: IndexSet) {
         todos.remove(atOffsets: offsets)
     }
+
+    private func saveTodos() {
+        guard let data = try? JSONEncoder().encode(todos) else { return
+        }
+        UserDefaults.standard.set(data, forKey: saveKey)
+    }
     
-//    func toggledone(_ todo: TodoItem) {
-//        guard let index = todos.firstIndex(where: {$0.id == todo.id }) else {
-//            return
-//        }
-//    }
+    private func loadTodos() {
+        guard let data = UserDefaults.standard.data(forKey: saveKey),
+              let savedTodos = try? JSONDecoder().decode([TodoItem].self, from: data)
+        else {
+            return
+        }
+        
+        todos = savedTodos
+    }
+    
+    func toggledone(_ todo: TodoItem) {
+        guard let index = todos.firstIndex(where: {$0.id == todo.id }) else {return}
+        todos[index].isDone.toggle()
+    }
 // 위 펑션은 RowView에서 바인딩이 아닌 뷰모델로 받아왔을 때 사용하는 함수
 }
