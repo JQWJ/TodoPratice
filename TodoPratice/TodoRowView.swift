ac860e90311506d7bb70c4e4c34eb6eacd84d96f
DELBE <jangjeong214@naver.com>
2026-01-25 13:57:50 +0900

    애니메이션 고도화
    고도화 했다

--- TodoPratice/TodoRowView.swift
+++ TodoPratice/TodoRowView.swift
@@ -15,6 +15,8 @@ struct TodoRowView: View {
     var body: some View {
         HStack {
                 Image(systemName: todo.isDone ? "checkmark.circle.fill" : "circle")
+                .scaleEffect(todo.isDone ? 1.1 : 1.0)
+                .animation(.spring(), value: todo.isDone)
                     .foregroundColor(todo.isDone ? .green : .gray)
                     .onTapGesture {
                         withAnimation(.spring()) {
