036c08eeac927a0cf3704a764488acabb212f553
DELBE <jangjeong214@naver.com>
2026-01-27 00:10:10 +0900

    Update todoItem
    우선순위 기능 추가하기 위한 구분자 생성

--- TodoPratice/TodoItem.swift
+++ TodoPratice/TodoItem.swift
@@ -7,8 +7,24 @@
 
 import SwiftUI
 
+enum TodoPriority: Int, Codable, CaseIterable {
+    case high = 0
+    case medium = 1
+    case low = 2
+    // int rawValue를 쓰는 이유는 정렬을 쉽게 하기 위해서임
+    
+    var title: String {
+        switch self {
+        case .high:  return "높음"
+        case .medium: return "중간"
+        case .low:   return "낮음"
+        }
+    }
+}
+
 struct TodoItem: Identifiable, Codable {
     let id: UUID
     var title: String
     var isDone: Bool
+    var priority: TodoPriority
 }
