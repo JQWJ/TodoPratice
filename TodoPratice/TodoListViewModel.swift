1d6374116a8a9a14ab73d475d57fe997a0c2e4d7
DELBE <jangjeong214@naver.com>
2026-01-27 00:12:35 +0900

    visibletodo 생성

--- TodoPratice/TodoListViewModel.swift
+++ TodoPratice/TodoListViewModel.swift
@@ -54,39 +54,9 @@ final class TodoListViewModel: ObservableObject {
             }
     }
     
-//    var visibleTodos: [TodoItem] {
-//        todos
-//        // 완료,미완료 필터
-//            .filter { todo in
-//                switch filter {
-//                case .all:
-//                    return true
-//                case .active:
-//                    return !todo.isDone
-//                case .completed:
-//                    return todo.isDone
-//                }
-//            }
-//            .filter {
-//                searchText.isEmpty ||
-//                $0.title.localizedCaseInsensitiveContains(searchText)
-//            }
-//            .sorted {
-//                !$0.isDone == false && $1.isDone == true
-//            }
-//    }
-    
-    private func sortTodos() {
-        todos.sort { lhs, rhs in
-            lhs.isDone == false && rhs.isDone == true
-        }
-    }
-    
-    func sortedTodos(
-        filter: TodoFilter,
-        searchText: String
-    ) -> [TodoItem] {
+    var visibleTodos: [TodoItem] {
         todos
+        // 완료,미완료 필터
             .filter { todo in
                 switch filter {
                 case .all:
@@ -97,14 +67,20 @@ final class TodoListViewModel: ObservableObject {
                     return todo.isDone
                 }
             }
-        
             .filter {
                 searchText.isEmpty ||
                 $0.title.localizedCaseInsensitiveContains(searchText)
             }
-        
-            .sorted {
-                !$0.isDone && $1.isDone
+            .sorted { lhs, rhs in
+                if lhs.isDone != rhs.isDone {
+                    return lhs.isDone == false
+                }
+                
+                if lhs.priority != rhs.priority {
+                    return lhs.priority.rawValue < rhs.priority.rawValue
+                }
+                
+                return true
             }
     }
     
@@ -114,8 +90,22 @@ final class TodoListViewModel: ObservableObject {
         loadTodos()
     }
     
-    func add(title: String) {
-        todos.append(TodoItem(id: UUID(), title: title, isDone: false))
+    func add(title: String, priority: TodoPriority) {
+        todos.append(
+            TodoItem(
+                id: UUID(),
+                title: title,
+                isDone: false,
+                priority: priority
+            )
+        )
+        
+        todos.sort { a, b in
+                if a.isDone != b.isDone {
+                    return !a.isDone
+                }
+                return a.priority.rawValue < b.priority.rawValue
+            }
     }
     
     func delete(at offsets: IndexSet) {
@@ -141,7 +131,12 @@ final class TodoListViewModel: ObservableObject {
     func toggledone(_ todo: TodoItem) {
             guard let index = todos.firstIndex(where: {$0.id == todo.id }) else {return}
             todos[index].isDone.toggle()
-        todos.sort { !$0.isDone && $1.isDone}
+        todos.sort { a, b in
+            if a.isDone != b.isDone {
+                return !a.isDone
+            }
+            return a.priority.rawValue < b.priority.rawValue
+        }
     }
 // 위 펑션은 RowView에서 바인딩이 아닌 뷰모델로 받아왔을 때 사용하는 함수
 }
